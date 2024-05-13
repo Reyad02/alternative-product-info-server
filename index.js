afac698bd80c241916d20a6d12ee016207a7f7cc
unknown <areyad203011@bscse.uiu.ac.bd>
2024-05-13 06:55:53 +0600

    get added function to find all qeries

--- index.js
+++ index.js
@@ -81,6 +81,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/queries', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
